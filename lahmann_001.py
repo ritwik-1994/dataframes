bd9aadd72ef563eb69069f64fda773852aff4c8c
ritwik-1994 <59331835+ritwik-1994@users.noreply.github.com>
2019-12-29 16:06:57 +0530

    Update lahmann_001.py

--- lahmann_001.py
+++ lahmann_001.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import requests
-from zipfile import ZipFile
+import zipfile
+import StringIO
 #salaries = pd.read_csv('/home/shipsy/Downloads/baseballdatabank-2019.2/core/Salaries.Csv')
 #teams = pd.read_csv('/home/shipsy/Downloads/baseballdatabank-2019.2/core/Teams.Csv')
 
 r = requests.get('http://seanlahman.com/files/database/lahman-csv_2014-02-14.zip')
-print(r.headers['content-type'])
-
+zipFile = zipfile.ZipFile(StringIO.StringIO(r.content))
+salariedDF = pd.read_csv(zipFile.open('Salaries.csv'))
+teamsDF = pd.read_csv(zipFile.open('Teams.csv'))
